e6683d7e6a793ab98c5fa0ab68b116d786229fa3
MerchantKhalid88 <mohammadkhr88@gmail;com>
2022-11-25 19:16:37 +0100

    error

--- src/Router/ErrorElement.js
+++ src/Router/ErrorElement.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 const ErrorElement = () => {
+    const navigate = useNavigate()
+    const goHome = () =>{
+        navigate('/')
+    }
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: `url("https://tse2.mm.bing.net/th?id=OIP.top-mfNIQ4kn7_mmm90DJgHaE0&pid=Api&P=0")` }}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="mb-5 text-5xl font-bold">Oops!!</h1>
+      <p className="mb-5">The page you are looking for cannot be found</p>
+      <button onClick={goHome}  className="btn btn-primary">Go Home Page</button>
     </div>
   </div>
 </div>
