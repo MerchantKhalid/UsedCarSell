e8bc9a1de097331b5e257e8a8b8660ef31f9fd8d
MerchantKhalid88 <mohammadkhr88@gmail;com>
2022-11-25 01:52:44 +0100

    modal

--- src/Pages/BookingModal/BookingModal.js
+++ src/Pages/BookingModal/BookingModal.js
@@ -13,10 +13,18 @@ const BookingModal = () => {
  <input type="checkbox" id="booking-modal" className="modal-toggle" />
         <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-            <h3 className="font-bold text-lg">{user.displayName}</h3>
-            <p className="py-4">{location}</p>
+        <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <h3 className='text-lg font-bold text-center mb-2'>Car Model:{model}</h3> 
+            <form className='text-center'>
+               <input type="text" placeholder="Type here" className="mb-3 input input-bordered w-full max-w-xs" />
+               <input type="text" placeholder="Type here" className="mb-3 input input-bordered w-full max-w-xs" />
+               <input type="text" placeholder="Type here" className="mb-3 input input-bordered w-full max-w-xs" />
+               <input type="text" placeholder="Type here" className="mb-3 input input-bordered w-full max-w-xs" />
+               <input type="text" placeholder="Type here" className="mb-3 input input-bordered w-full max-w-xs" />
+               <input type='submit' value='Submit' className='w-full max-w-xs btn btn-dark'></input>
+            </form>
             <div className="modal-action">
-            <label htmlFor="booking-modal" className="btn">X</label>
+            
             </div>
         </div>
         </div>
